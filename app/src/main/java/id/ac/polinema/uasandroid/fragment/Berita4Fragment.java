eff754747fba9528df072e68ade9569d9897698c
Rizkyyoga <rizkyyoga211@gmail.com>
2019-12-15 23:55:33 +0700

    BeritaFragments

--- app/src/main/java/id/ac/polinema/uasandroid/fragment/Berita4Fragment.java
+++ app/src/main/java/id/ac/polinema/uasandroid/fragment/Berita4Fragment.java
@@ -9,11 +9,12 @@ import androidx.fragment.app.Fragment;
 
 import id.ac.polinema.uasandroid.R;
 
-public class HomeFragment extends Fragment {
+public class Berita4Fragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_berita4, container, false);
     }
 }
--- app/src/main/java/id/ac/polinema/uasandroid/fragment/BeritaFragment.java
+++ app/src/main/java/id/ac/polinema/uasandroid/fragment/BeritaFragment.java
@@ -1,19 +1,101 @@
 package id.ac.polinema.uasandroid.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import id.ac.polinema.uasandroid.R;
+import id.ac.polinema.uasandroid.activities.DeskripsiActivity;
 
 public class BeritaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_berita, container, false);
+
+        CardView cardView = (CardView) view.findViewById(R.id.berita1);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                Berita1Fragment b = new Berita1Fragment();
+                b.setArguments(bundle);
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, b)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        CardView cardView1 = (CardView) view.findViewById(R.id.berita2);
+        cardView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                Berita2Fragment a = new Berita2Fragment();
+                a.setArguments(bundle);
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, a)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        CardView cardView2 = (CardView) view.findViewById(R.id.berita3);
+        cardView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                Berita3Fragment c = new Berita3Fragment();
+                c.setArguments(bundle);
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, c)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        CardView cardView3 = (CardView) view.findViewById(R.id.berita4);
+        cardView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                Berita4Fragment d = new Berita4Fragment();
+                d.setArguments(bundle);
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, d)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        CardView cardView4 = (CardView) view.findViewById(R.id.berita5);
+        cardView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                Berita5Fragment f = new Berita5Fragment();
+                f.setArguments(bundle);
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, f)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_berita, container, false);
+
+        return view;
     }
+
+    // TODO: Rename method, update argument and hook method into UI event
+
 }
+
